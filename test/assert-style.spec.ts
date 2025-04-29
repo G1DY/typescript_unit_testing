c0646afa5826d4d82862d9300c0e153b197b7155
gideon <kipkerring.gedion@gmail.com>
2025-04-29 20:39:28 +0300

    assert style testing

--- test/assert-style.spec.ts
+++ test/assert-style.spec.ts
@@ -11,5 +11,19 @@ describe('Test Cases using assert-style', () => {
     expect(name).to.be.ok;
     expect(name).to.equal('Gideon');
     expect(name).to.be.a('string');
+
+    expect(age).to.be.ok;
+    expect(age).to.be.equal(30);
+    expect(age).to.be.a('number');
+
+    expect(objData).to.be.ok;
+    expect(objData).to.have.property('name').equal('Gideon');
+    expect(objData).to.have.property('age').equal(30);
+    expect(objData).to.be.a('Object');
+
+    expect(arrData).to.be.ok;
+    expect(arrData).to.have.lengthOf(3);
+    expect(arrData).to.have.lengthOf(3).include(2);
+    expect(arrData).to.be.a('Array');
   });
 });
